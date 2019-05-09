3e72833c6a1a0808638514a22197ff0b61e299d8
huynhhoangkha <huynhhoangkha@gmail.com>
2019-05-09 12:28:05 +0700

    Tao duoc bang person

--- MySQL.sql
+++ MySQL.sql
@@ -2,50 +2,18 @@
 CREATE DATABASE TickLabInfoSystem;
 USE TickLabInfoSystem;
 -- CREATING TABLE
-    -- TickLab ID CARD
-        CREATE TABLE TickLabIDCard (
-            profileNumber int,
-            dateOfIssue datetime,
-            rfidNumber int,
-            PRIMARY KEY(rfidNumber)
-        );
-    -- Contact address
-        CREATE TABLE PersonContactAddress (
-            profileNumber int,
-            contactAddress nchar
-        );
-    -- Email address
-        CREATE TABLE PersonEmailAddress (
-            profileNumber int,
-            emailAddress char
-        );
-    -- System roles
-        CREATE TABLE PersonSystemRole (
-            profileNumber int,
-            systemRole char
-        );
-    -- Related documents
-        CREATE TABLE PersonRelatedDoc (
-            profileNumber int,
-            documentURL char
-        );
-    -- Person phone number
-        CREATE TABLE PersonPhoneNumber (
-            profileNumber int,
-            phoneNumber char
-        );
     -- Person
         CREATE TABLE Person (
-            profileNumber int,
+            profileNumber int NOT NULL,
             username char,
             passwd char,
             gender boolean,
-            permanentAddress nchar,
+            permanentAddress char,
             dateOfBirth datetime,
-            firstName nchar,
-            middleName nchar,
-            lastName nchar,
-            nationality nchar,
+            firstName char,
+            middleName char,
+            lastName char,
+            nationality char,
             nationalIDNumber char,
             nationalIDIssueDate datetime,
             passportNumber char,
@@ -54,4 +22,42 @@ USE TickLabInfoSystem;
             passportDateOfExpiry datetime,
             profilePhotoURL char,
             PRIMARY KEY(profileNumber)
+        );
+    -- TickLab ID CARD
+        CREATE TABLE TickLabIDCard (
+            profileNumber int,
+            dateOfIssue datetime,
+            rfidNumber int,
+            PRIMARY KEY(rfidNumber),
+            FOREIGN KEY(profileNumber) REFERENCES Person(profileNumber)
+        );
+    -- Contact address
+        CREATE TABLE PersonContactAddress (
+            profileNumber int,
+            contactAddress char,
+            FOREIGN KEY(profileNumber) REFERENCES Person(profileNumber)
+        );
+    -- Email address
+        CREATE TABLE PersonEmailAddress (
+            profileNumber int,
+            emailAddress char,
+            FOREIGN KEY(profileNumber) REFERENCES Person(profileNumber)
+        );
+    -- System roles
+        CREATE TABLE PersonSystemRole (
+            profileNumber int,
+            systemRole char,
+            FOREIGN KEY(profileNumber) REFERENCES Person(profileNumber)
+        );
+    -- Related documents
+        CREATE TABLE PersonRelatedDoc (
+            profileNumber int,
+            documentURL char,
+            FOREIGN KEY(profileNumber) REFERENCES Person(profileNumber)
+        );
+    -- Person phone number
+        CREATE TABLE PersonPhoneNumber (
+            profileNumber int,
+            phoneNumber char,
+            FOREIGN KEY(profileNumber) REFERENCES Person(profileNumber)
         );
