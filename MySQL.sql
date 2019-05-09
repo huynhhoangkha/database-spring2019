9d88b4fcbe90fe3631667d01e87b8f1e56162b3c
huynhhoangkha <huynhhoangkha@gmail.com>
2019-05-09 09:39:16 +0700

    person, chua xong

--- MySQL.sql
+++ MySQL.sql
@@ -1,5 +1,6 @@
 -- CREATING DATABASE
 CREATE DATABASE TickLabInfoSystem;
+USE TickLabInfoSystem;
 -- CREATING TABLE
     -- TickLab ID CARD
         CREATE TABLE TickLabIDCard (
@@ -7,6 +8,50 @@ CREATE DATABASE TickLabInfoSystem;
             dateOfIssue datetime,
             rfidNumber int,
             PRIMARY KEY(rfidNumber)
-        )
+        );
     -- Contact address
-        
+        CREATE TABLE PersonContactAddress (
+            profileNumber int,
+            contactAddress nchar
+        );
+    -- Email address
+        CREATE TABLE PersonEmailAddress (
+            profileNumber int,
+            emailAddress char
+        );
+    -- System roles
+        CREATE TABLE PersonSystemRole (
+            profileNumber int,
+            systemRole char
+        );
+    -- Related documents
+        CREATE TABLE PersonRelatedDoc (
+            profileNumber int,
+            documentURL char
+        );
+    -- Person phone number
+        CREATE TABLE PersonPhoneNumber (
+            profileNumber int,
+            phoneNumber char
+        );
+    -- Person
+        CREATE TABLE Person (
+            profileNumber int,
+            username char,
+            passwd char,
+            gender boolean,
+            permanentAddress nchar,
+            dateOfBirth datetime,
+            firstName nchar,
+            middleName nchar,
+            lastName nchar,
+            nationality nchar,
+            nationalIDNumber char,
+            nationalIDIssueDate datetime,
+            passportNumber char,
+            passportPlaceOfIssue char,
+            passportDateOfIssue datetime,
+            passportDateOfExpiry datetime,
+            profilePhotoURL char,
+            PRIMARY KEY(profileNumber)
+        );
