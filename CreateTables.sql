17dc51c3946894c19c94d68d5a37108fe242c583
Chau Thanh Dat <ctd@ticklab.vn>
2019-05-20 20:29:54 +0700

    Edit participant

--- CreateTables.sql
+++ CreateTables.sql
@@ -223,7 +223,7 @@ USE TickLabInfoSystem;
         fromDate DATE,
         toDate DATE,
         PRIMARY KEY(participantID, taskID),
-        FOREIGN KEY(participantID) REFERENCES Participate(participantID),
+        FOREIGN KEY(participantID) REFERENCES Participant(participantID),
         FOREIGN KEY(taskID) REFERENCES Task(taskID)
     );
     -- Task remake
