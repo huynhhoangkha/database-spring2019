d0ce9a5566615b39569a1e47cb47b983ff81b95a
Huynh Hoang Kha <huynhhoangkha@gmail.com>
2019-05-21 03:19:35 +0700

    get phone number from email

--- Procedures.sql
+++ Procedures.sql
@@ -1,3 +1,4 @@
+-- Kha
 -- Insert to WorkPosition--
 CREATE PROC workPosIns
     @posID INT,
@@ -18,3 +19,15 @@ AS
     END
 GO
 ---------------------------
+
+-- Get phone number from email----
+ALTER PROCEDURE getPhoneNumberByEmail
+	@email VARCHAR(256)
+AS
+BEGIN
+	DECLARE
+		@profileNumber INT
+	SELECT @profileNumber = profileNumber FROM PersonEmailAddress WHERE emailAddress = @email
+	SELECT phoneNumber FROM PersonPhoneNumber WHERE profileNumber = @profileNumber
+END
+-----------------------------------
